50341cd14b146b82fda7a89255f4b494cf4cc550
Seweryn <ssevve@gmail.com>
2020-11-01 17:03:10 +0100

    Add desktop nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import image from '../../coins.png';
 
@@ -13,6 +14,16 @@ function Header() {
             <img className="header__logo-image" src={image} alt="" />
             <h1 className="header__logo-text">OSRS Flip Tracker</h1>
           </div>
+          <nav className="nav">
+            <ul className="nav__desktop-menu">
+              <li className="nav__desktop-item">
+                <NavLink className="nav__desktop-link" activeClassName="nav__desktop-link--active" exact to="/">Active offers</NavLink>
+              </li>
+              <li className="nav__desktop-item">
+                <NavLink className="nav__desktop-link" activeClassName="nav__desktop-link--active" to="/history">History</NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
 			</header>
   )

36f4404eba24f775c7b76f4f0ab0c7d93a0060a4
Seweryn <ssevve@gmail.com>
2020-11-05 10:21:36 +0100

    modify test flips

--- src/App.jsx
+++ src/App.jsx
@@ -10,138 +10,147 @@ function App() {
   const [flips, setFlips] = useState([
     {
       itemName: 'item1',
-      buyPrice: '255',
+      buyPrice: '40',
       quantity: '10',
-      sellPrice: '256',
+      sellPrice: '50',
       isComplete: true,
-      createdAt: 1604403861068,
-      id: 34141,
+      createdAt: 1604566749860,
+      id: 65463243
     },
     {
       itemName: 'item2',
       buyPrice: '40',
       quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604403861068,
-      id: 2325423
-    },
-    {
-      itemName: 'item3',
-      buyPrice: '44',
-      quantity: '101',
-      sellPrice: '54',
-      isComplete: true,
-      createdAt: 1604403861068,
-      id: 32342
-    },
-    {
-      itemName: 'item4',
-      buyPrice: '50',
-      quantity: '110',
-      sellPrice: '69',
-      isComplete: true,
-      createdAt: 1604403861068,
-      id: 454
-    },
-    {
-      itemName: 'item5',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604403861068,
-      id: 5546
-    },
-    {
-      itemName: 'item6',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604319660803,
-      id: 6546
-    },
-    {
-      itemName: 'item7',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604319660803,
-      id: 7654
-    },
-    {
-      itemName: 'item8',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604319660803,
-      id: 8345
-    },
-    {
-      itemName: 'item9',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604319660803,
-      id: 9765
-    },
-    {
-      itemName: 'item10',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '45',
-      isComplete: true,
-      createdAt: 1604319660803,
-      id: 10345
-    },
-    {
-      itemName: 'item11',
-      buyPrice: '40',
-      quantity: '10',
-      sellPrice: '50',
+      sellPrice: '44',
       isComplete: true,
       createdAt: 1604490844514,
       id: 654632
     },
     {
-      itemName: 'item12',
+      itemName: 'item3',
       buyPrice: '40',
       quantity: '10',
-      sellPrice: '50',
+      sellPrice: '44',
       isComplete: true,
       createdAt: 1604490844514,
       id: 7654345
     },
     {
-      itemName: 'item13',
+      itemName: 'item4',
       buyPrice: '40',
       quantity: '10',
-      sellPrice: '50',
+      sellPrice: '44',
       isComplete: true,
       createdAt: 1604490844514,
       id: 8345345
     },
     {
-      itemName: 'item14',
+      itemName: 'item5',
       buyPrice: '40',
       quantity: '10',
-      sellPrice: '50',
+      sellPrice: '44',
       isComplete: true,
       createdAt: 1604490844514,
       id: 976554
     },
     {
-      itemName: 'item15',
+      itemName: 'item6',
       buyPrice: '40',
       quantity: '10',
-      sellPrice: '50',
+      sellPrice: '44',
       isComplete: true,
       createdAt: 1604490844514,
       id: 1034534
+    },
+    {
+      itemName: 'item7',
+      buyPrice: '255',
+      quantity: '10',
+      sellPrice: '261',
+      isComplete: true,
+      createdAt: 1604403861068,
+      id: 34141,
+    },
+    {
+      itemName: 'item8',
+      buyPrice: '255',
+      quantity: '10',
+      sellPrice: '261',
+      isComplete: true,
+      createdAt: 1604403861068,
+      id: 2325423
+    },
+    {
+      itemName: 'item9',
+      buyPrice: '255',
+      quantity: '10',
+      sellPrice: '261',
+      isComplete: true,
+      createdAt: 1604403861068,
+      id: 32342
+    },
+    {
+      itemName: 'item10',
+      buyPrice: '255',
+      quantity: '10',
+      sellPrice: '261',
+      isComplete: true,
+      createdAt: 1604403861068,
+      id: 454
+    },
+    {
+      itemName: 'item11',
+      buyPrice: '255',
+      quantity: '10',
+      sellPrice: '261',
+      isComplete: true,
+      createdAt: 1604403861068,
+      id: 5546
+    },
+    {
+      itemName: 'item12',
+      buyPrice: '40',
+      quantity: '10',
+      sellPrice: '48',
+      isComplete: true,
+      createdAt: 1604319660803,
+      id: 6546
+    },
+    {
+      itemName: 'item13',
+      buyPrice: '40',
+      quantity: '10',
+      sellPrice: '48',
+      isComplete: true,
+      createdAt: 1604319660803,
+      id: 7654
+    },
+    {
+      itemName: 'item14',
+      buyPrice: '40',
+      quantity: '10',
+      sellPrice: '48',
+      isComplete: true,
+      createdAt: 1604319660803,
+      id: 8345
+    },
+    {
+      itemName: 'item15',
+      buyPrice: '40',
+      quantity: '10',
+      sellPrice: '48',
+      isComplete: true,
+      createdAt: 1604319660803,
+      id: 9765
+    },
+    {
+      itemName: 'item16',
+      buyPrice: '40',
+      quantity: '10',
+      sellPrice: '48',
+      isComplete: true,
+      createdAt: 1604319660803,
+      id: 10345
     },
   ]);
 

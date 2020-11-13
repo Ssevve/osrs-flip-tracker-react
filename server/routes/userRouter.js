9516308d44114c9c5e2fdb3fed509261065e9646
Seweryn <ssevve@gmail.com>
2020-11-13 13:37:36 +0100

    Refactor login route

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,13 +4,32 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const User = require('../models/userModel');
 
+const schema = Joi.object({
+	username: Joi.string().alphanum().min(4).max(30).trim().required(),
+	password: Joi.string().min(8).trim().required(),
+});
+
+const respondError409 = (res) => {
+	res.status(409).json({ message: 'Unable to login.' });
+};
+
+// const generateAndSendToken = (user, res) => {
+// 	const payload = {
+// 		_id: user._id,
+// 		username: user.username,
+// 	}
+// 	jwt.sign(payload, process.env.JWT_SECRET, {
+// 		expiresIn: '1d',
+// 	}, (err, token) => {
+// 		if (err) res.status(400).json({ message: 'Failed to generate a token. Try again.' });
+// 		res.json({
+// 			token,
+// 		});
+// 	});
+// }
+
 router.post('/signup', async (req, res) => {
 	try {
-		const schema = Joi.object({
-			username: Joi.string().alphanum().min(4).max(30).trim().required(),
-			password: Joi.string().min(8).trim().required(),
-		});
-
 		// Validate
 		const result = schema.validate(req.body);
 		if (!result.error) {
@@ -51,35 +70,38 @@ router.post('/signup', async (req, res) => {
 
 router.post('/login', async (req, res) => {
 	try {
-		const { email, password } = req.body;
-
-		if (!email || !password) {
-			return res.status(400).json({ msg: 'All fields are required.' });
+		const result = schema.validate(req.body);
+		if (!result.error) {
+			// Check if user exists
+			const existingUser = await User.findOne({ username: req.body.username });
+			if (existingUser) {
+				// Compare the password
+				bcrypt.compare(req.body.password, existingUser.password).then(match => {
+					if (match) {
+						// Passwords match
+						// Generate and send the token
+						const payload = {
+							_id: existingUser._id,
+							username: existingUser.username,
+						}
+						jwt.sign(payload, process.env.JWT_SECRET, {
+							expiresIn: '1d',
+						}, (err, token) => {
+							if (err) respondError409(res);
+							res.json({
+								token,
+							});
+						});
+					} else {
+						respondError409(res);
+					}
+				});				
+			} else {
+				respondError409(res);
+			}
+		} else {
+			respondError409(res);
 		}
-
-		// Check if user exists
-		const user = await User.findOne({ email });
-		if (!user) {
-			return res
-				.status(400)
-				.json({ msg: 'There is no account with this email address.' });
-		}
-
-		// Check if password is correct
-		const isMatch = await bcrypt.compare(password, user.password);
-		if (!isMatch) {
-			return res.status(400).json({ msg: 'Invalid password.' });
-		}
-
-		// Generate JWT token
-		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-		res.json({
-			token,
-			user: {
-				id: user._id,
-				displayName: user.displayName,
-			},
-		});
 	} catch (err) {
 		return res.status(500).json({ error: err.message });
 	}
